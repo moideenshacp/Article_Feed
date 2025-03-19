20b6b6fd9541d98a72374195da969bcb1661cdb6
Moideensha CP <your-email@example.com>
2025-03-19 10:37:01 +0530

    removed unwated logs and add autheticate in abckend

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -11,7 +11,7 @@ export const authenticate = (req: Request, res: Response, next: NextFunction) =>
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET || "your_secret_key");
         (req as any).user = decoded;
-        next();
+        next();   
     } catch (error) {
         res.status(401).json({ message: "Invalid token." });
     }
