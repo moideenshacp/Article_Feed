27a55e01ff344247878fa9506fb90ced2be74898
Moideensha CP <your-email@example.com>
2025-03-19 11:00:01 +0530

    removed autheticate

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,11 +1,10 @@
 import express from 'express'
 import  { signIn, signUp, updateUserProfile } from '../controllers/authController'
-import { authenticate } from '../middleware/authMiddleware'
 
 const router = express.Router()
 
 router.post('/sign-up',signUp)
 router.post('/sign-in',signIn)
-router.patch('/update-profile',authenticate, updateUserProfile)
+router.patch('/update-profile', updateUserProfile)
 
 export default router
